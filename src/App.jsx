650d50b1c07713f18b9427a870e8dbba025e8bb9
Govindaraj-web <arjungovi.webcreator@gmail.com>
2025-08-13 16:14:08 +0530

    update logo,animation and active

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import LandingPage from "./components/LandingPage";
 import { HelmetProvider } from "react-helmet-async";
 
+
 export default function App() {
   return (
     <HelmetProvider>
